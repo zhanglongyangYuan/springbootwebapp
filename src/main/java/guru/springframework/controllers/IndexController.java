0371f913f12c75e589447920fff86b1f4f59a4fc
张隆洋 <longyang.zhang@lkcoffee.com>
2023-03-25 10:45:54 +0800

    0-1建设

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     String index(){
-        return "index";
+        System.out.println(".....");
+        return "redirect:/index.html";
     }
 }
